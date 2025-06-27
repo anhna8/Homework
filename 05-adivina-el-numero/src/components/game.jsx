59abbba5bbf848e4faaa30ddc7aa4bf1194b3764
anhna <ana_mjl@hotmail.com>
2025-06-27 16:26:09 -0600

    commit juego

---
+++ 05-adivina-el-numero/src/components/game.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import InputNumber from './inputNumber';
+import Message from './message';
+import RestartButton from './restartButton';
+import './App.css';
+
+
+function Game() {
+  const [numeroSecreto, setNumeroSecreto] = useState(generarNumero());
+  const [intento, setIntento] = useState('');
+  const [mensaje, setMensaje] = useState('');
+  const [ganaste, setGanaste] = useState(false);
+
+  function generarNumero() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+  function verificarNumero() {
+    const num = parseInt(intento, 10);
+    if (num === numeroSecreto) {
+      setMensaje('¡Correcto! 🎉');
+      setGanaste(true);
+    } else if (num < numeroSecreto) {
+      setMensaje('El número es mayor 🔺');
+    } else {
+      setMensaje('El número es menor 🔻');
+    }
+  }
+
+  function reiniciar() {
+    setNumeroSecreto(generarNumero());
+    setIntento('');
+    setMensaje('');
+    setGanaste(false);
+  }
+
+  return (
+    <div className="juego">
+      <h1>Adivina el Número 🎲</h1>
+      <InputNumber intento={intento} setIntento={setIntento} onCheck={verificarNumero} disabled={ganaste} />
+      <Message mensaje={mensaje} />
+      {ganaste && <RestartButton onRestart={reiniciar} />}
+    </div>
+  );
+}
+
+export default Game;
