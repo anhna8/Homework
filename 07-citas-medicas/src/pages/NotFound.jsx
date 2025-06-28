d445b60e4d4bd867f00831252ea86fe1dc8bbd27
anhna <ana_mjl@hotmail.com>
2025-06-27 18:59:43 -0600

    created base estructure

---
+++ 07-citas-medicas/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h2>¡Página no encontrada! 😅</h2>
+      <p>La ruta que estás buscando no existe.</p>
+    </div>
+  );
+}
+
+export default NotFound;
